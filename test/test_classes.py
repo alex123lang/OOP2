f2b8af57f5d498629c663bcd708bbd4ad2a0047c
Russel <russelkas6@gmail.com>
2024-02-09 16:19:34 +0300

    fixed test_classes.py

--- test/test_classes.py
+++ test/test_classes.py
@@ -3,18 +3,18 @@ from src.product import Product
 
 
 def test_category():
-    category = Category("test", "test", [])
+    category = Category("test", "test description", ['product 1', 'product 2'])
     assert category.name == "test"
-    assert category.description == "test"
-    assert category.products == []
-    assert category.category_quantity == 0
-    assert category.total_quantity == 0
+    assert category.description == "test description"
+    assert category.products == ['product 1', 'product 2']
+    assert Category.category_quantity == 1
+    assert Category.total_quantity == 2
 
 
 def test_product():
-    product = Product("test", "test", 0, 0)
+    product = Product("test", "test description", 10_000, 5)
     assert product.name == "test"
-    assert product.description == "test"
-    assert product.price == 0
-    assert product.product_quantity == 0
+    assert product.description == "test description"
+    assert product.price == 10_000
+    assert product.product_quantity == 5
 
