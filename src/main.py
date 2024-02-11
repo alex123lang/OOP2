59e56615e1db48f9e81f3e04f88d1634a093dac9
Russel <russelkas6@gmail.com>
2024-02-11 18:44:49 +0300

    fixed main.py

--- src/main.py
+++ src/main.py
@@ -5,15 +5,25 @@ from product import Product
 
 def main():
     data = products
+    categories = []
     for i in data:
-        category = Category(i["name"], i["description"], [j["name"] for j in i["products"]])
-        print(f'{category.name}\n{category.description}\n{category.products}\n'
-              f'Количество категорий: {Category.category_quantity}\n'
-              f'Количество продуктов: {Category.total_quantity}\n')
+        products_in_category = []
 
         for j in i["products"]:
             product = Product(j["name"], j["description"], j["price"], j["quantity"])
+            products_in_category.append(product)
+
+        category = Category(i["name"], i["description"], products_in_category)
+        categories.append(category)
+
+    for category in categories:
+        print(f'{category.name}\n{category.description}\n{category.format_products}\n')
+
+        for product in category.products:
             print(f'{product.name}\n{product.description}\n{product.price}\n{product.product_quantity}\n')
+
+    print(f'Количество категорий: {Category.category_quantity}\n'
+          f'Количество продуктов: {Category.total_quantity}\n')
 
 
 if __name__ == "__main__":
