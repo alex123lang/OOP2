f8abc7a2b8d749dbee9180fb854f586dcccdf174
Russ <ruzilkasymov5@gmail.com>
2024-02-23 16:53:33 +0300

    added 2 classes and added functional to __add__

--- src/product.py
+++ src/product.py
@@ -29,4 +29,35 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.product_quantity} шт."
 
     def __add__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError
         return (self.price * self.product_quantity) + (other.price * other.product_quantity)
+
+
+class Smartphones(Product):
+
+    efficiency: float
+    model: str
+    internal_memory: int
+    colour: str
+
+    def __init__(self, name, description, price, product_quantity, efficiency, model, internal_memory, colour):
+        super().__init__(name, description, price, product_quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.internal_memory = internal_memory
+        self.colour = colour
+
+
+class LawnGrass(Product):
+
+    made_in: str
+    germination: int
+    colour: str
+
+    def __init__(self, name, description, price, product_quantity, made_in, germination, colour):
+        super().__init__(name, description, price, product_quantity)
+        self.made_in = made_in
+        self.germination = germination
+        self.colour = colour
+
